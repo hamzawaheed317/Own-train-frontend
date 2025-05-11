fea3a3f1c5fffc7c29c398d6964182e48e14b5e0
talhariaz105 <171585975+hamzawaheed317@users.noreply.github.com>
2025-05-11 15:24:50 +0500

    update code

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import dotenv from "dotenv";
+dotenv.config(); // Load .env variables
+import path from "path";
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  define: {
+    "import.meta.env.REACT_APP_API_URL": JSON.stringify(
+      process.env.BACKEND_BASE_URL
+    ),
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    host: true,
+  },
+  plugins: [react()],
 });
